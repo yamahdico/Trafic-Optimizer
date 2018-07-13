e90422d505646a2ea59309993911828167a819ce
fadak.ir <yamahdico@yahoo.com>
2018-07-13 12:24:20 +0300

    v0.1

--- optimized.js
+++ optimized.js
@@ -1,31 +1,23 @@
-
-sessionStorage.setItem('key', 'value');
-
-var data = sessionStorage.getItem('key');
-
-
-console.log(data);
+console.log(sessionStorage.getItem('active'));
 
 browser.browserAction.onClicked.addListener(cancel2);
 
+cancel2();
 
 //ارتباط با حافظه
 function cancel2() {
-chrome.storage.local.get("active", items=>{
-	console.log(items.active);
-	active = items.active;
 	
-	if(items.active){
-	browser.storage.local.set({active:false});
-	  browser.browserAction.setIcon({path: "Tele2.ico"});
+	console.log(sessionStorage.getItem('active'));
+	
+	if(sessionStorage.getItem('active')=='true'){
+	sessionStorage.setItem('active', 'false');	
+	browser.browserAction.setIcon({path: "Tele2.ico"});
+	console.log("ssss");
 	}else{
-	browser.storage.local.set({active:true});
+	sessionStorage.setItem('active', 'true');
 	browser.browserAction.setIcon({path: "Not_optimum.png"});
 	}
-	});
 }
-
-
 
 // match pattern for the URLs to redirect
 var pattern = "<all_urls>";
@@ -34,19 +26,12 @@ var pattern1 = ".ansarbank.com/ .antiplagiat.ru .bale.ai .eitaa.com/ .libgen.pw/
 // cancel function returns an object
 // which contains a property `cancel` set to `true`
 function cancel(requestDetails) {
-	
-	
-		console.log(active);
-	
- // console.log("Cancelingaa: " + requestDetails.url.substring(2));
-if(pattern1.indexOf(getDomain(requestDetails.url))<0 && active){
-	 console.log("Canceling: " + getDomain(requestDetails.url));
-	 return {cancel: true};
-}else{
-		 console.log("Allow: " + getDomain(requestDetails.url));
-
-	
-}
+	if(pattern1.indexOf(getDomain(requestDetails.url))<0 && sessionStorage.getItem('active') == 'false'){
+		 console.log("Canceling: " + getDomain(requestDetails.url));
+		 return {cancel: true};
+	}else{
+			 console.log("Allow: " + getDomain(requestDetails.url));
+	}
 }
 
 // add the listener,
@@ -57,7 +42,6 @@ browser.webRequest.onBeforeRequest.addListener(
   ["blocking"]
 );
 
-
 function getHostName(url) {
     var match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
     if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) {
@@ -67,6 +51,7 @@ function getHostName(url) {
         return null;
     }
 }
+
 function getDomain(url) {
     var hostName = getHostName(url);
     var domain = hostName;
